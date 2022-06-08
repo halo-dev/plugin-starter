2691a0394d70e3ed2ec9f3526ab65a2a51836a24
guqing <i@guqing.email>
2022-06-08 16:01:07 +0800

    init commit

---
+++ src/main/java/io/github/guqing/apples/ApplesController.java
@@ -0,0 +1,20 @@
+package io.github.guqing.apples;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author guqing
+ * @since 2.0.0
+ */
+@RestController
+@RequestMapping("/apis/apples.guqing.github.com/v1alpha1/apples")
+public class ApplesController {
+
+    @GetMapping
+    public Mono<String> hello() {
+        return Mono.just("Hello world");
+    }
+}
