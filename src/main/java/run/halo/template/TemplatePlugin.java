6651fb995b3f07de123137eb293e9ab9bbc8704d
guqing <i@guqing.email>
2022-10-08 11:03:30 +0800

    refactor: using halo plugin gradle plugin

--- src/main/java/run/halo/template/TemplatePlugin.java
+++ src/main/java/run/halo/template/TemplatePlugin.java
@@ -1,6 +1,7 @@
 package run.halo.template;
 
 import org.pf4j.PluginWrapper;
+import org.springframework.stereotype.Component;
 import run.halo.app.extension.Scheme;
 import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
@@ -9,12 +10,13 @@ import run.halo.app.plugin.BasePlugin;
  * @author guqing
  * @since 2.0.0
  */
+@Component
 public class TemplatePlugin extends BasePlugin {
     private final SchemeManager schemeManager;
 
-    public TemplatePlugin(PluginWrapper wrapper) {
+    public TemplatePlugin(PluginWrapper wrapper, SchemeManager schemeManager) {
         super(wrapper);
-        this.schemeManager = getApplicationContext().getBean(SchemeManager.class);
+        this.schemeManager = schemeManager;
     }
 
     @Override
