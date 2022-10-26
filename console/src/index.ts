c6d1006c0fc0c754e2119e23515274bb6d9545fb
Ryan Wang <i@ryanc.cc>
2022-10-26 18:05:39 +0800

    chore: bump dependencies

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,40 +1,42 @@
-import { definePlugin, BasicLayout } from "@halo-dev/console-shared";
+import {definePlugin} from "@halo-dev/console-shared";
 import DefaultView from "./views/DefaultView.vue";
-import { IconGrid } from "@halo-dev/components";
+import {IconGrid} from "@halo-dev/components";
 import "./styles/index.css";
+import {markRaw} from "vue";
 
 export default definePlugin({
   name: "PluginStarter",
   components: [],
   routes: [
     {
-      path: "/hello-world",
-      component: BasicLayout,
-      children: [
+      parentName: "Root",
+      route:
         {
-          path: "",
-          name: "HelloWorld",
-          component: DefaultView,
-          meta: {
-            permissions: ["plugin:apples:view"],
-          },
-        },
-      ],
-    },
-  ],
-  menus: [
-    {
-      name: "From PluginStarter",
-      items: [
-        {
-          name: "HelloWorld",
           path: "/hello-world",
-          icon: IconGrid,
+          children: [
+            {
+              path: "",
+              name: "HelloWorld",
+              component: DefaultView,
+              meta: {
+                permissions: ["plugin:apples:view"],
+                title: "HelloWorld",
+                searchable: true,
+                menu: {
+                  name: "迁移",
+                  group: "From PluginStarter",
+                  icon: markRaw(IconGrid),
+                  priority: 0,
+                },
+              },
+            },
+          ],
         },
-      ],
-    },
+    }
   ],
   extensionPoints: {},
-  activated() {},
-  deactivated() {},
+  activated() {
+  },
+  deactivated() {
+  },
 });
