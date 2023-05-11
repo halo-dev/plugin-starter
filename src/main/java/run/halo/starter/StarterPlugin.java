8f90a905a26cf3f2abccdf5bbcc2569c7a716d90
guqing <i@guqing.email>
2023-05-11 12:03:03 +0800

    refactor: optimizing project structure and configuration

--- src/main/java/run/halo/starter/StarterPlugin.java
+++ src/main/java/run/halo/starter/StarterPlugin.java
@@ -2,13 +2,15 @@ package run.halo.starter;
 
 import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
-import run.halo.app.extension.Scheme;
-import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 
 /**
+ * <p>Plugin main class to manage the lifecycle of the plugin.</p>
+ * <p>This class must be public and have a public constructor.</p>
+ * <p>Only one main class extending {@link BasePlugin} is allowed per plugin.</p>
+ *
  * @author guqing
- * @since 2.0.0
+ * @since 1.0.0
  */
 @Component
 public class StarterPlugin extends BasePlugin {
