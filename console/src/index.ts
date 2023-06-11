211507bf362a137e6593391b9465dbcd29b35940
Ryan Wang <i@ryanc.cc>
2023-06-11 20:02:30 +0800

    chore: improve the front-end infrastructure
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/src/index.ts
+++ console/src/index.ts
@@ -10,23 +10,18 @@ export default definePlugin({
       parentName: "Root",
       route: {
         path: "/example",
-        children: [
-          {
-            path: "",
-            name: "Example",
-            component: HomeView,
-            meta: {
-              title: "示例页面",
-              searchable: true,
-              menu: {
-                name: "示例页面",
-                group: "示例分组",
-                icon: markRaw(IconPlug),
-                priority: 0,
-              },
-            },
+        name: "Example",
+        component: HomeView,
+        meta: {
+          title: "示例页面",
+          searchable: true,
+          menu: {
+            name: "示例页面",
+            group: "示例分组",
+            icon: markRaw(IconPlug),
+            priority: 0,
           },
-        ],
+        },
       },
     },
   ],
--- console/vite.config.ts
+++ console/vite.config.ts
@@ -8,40 +8,54 @@ import Icons from "unplugin-icons/vite";
 const pluginEntryName = "PluginStarter";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [Vue(), VueJsx(), Icons({ compiler: "vue3" })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    outDir: fileURLToPath(
-      new URL("../src/main/resources/console", import.meta.url)
-    ),
-    emptyOutDir: true,
-    lib: {
-      entry: "src/index.ts",
-      name: pluginEntryName,
-      formats: ["iife"],
-      fileName: () => "main.js",
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "@halo-dev/console-shared",
-        "@halo-dev/components",
-        "vue-router",
-      ],
-      output: {
-        globals: {
-          vue: "Vue",
-          "vue-router": "VueRouter",
-          "@halo-dev/components": "HaloComponents",
-          "@halo-dev/console-shared": "HaloConsoleShared",
-        },
-        extend: true,
+export default ({ mode }: { mode: string }) => {
+  const isProduction = mode === "production";
+  const outDir = isProduction
+    ? "../src/main/resources/console"
+    : "../build/resources/main/console";
+
+  return defineConfig({
+    plugins: [Vue(), VueJsx(), Icons({ compiler: "vue3" })],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
-});
+    define: {
+      "process.env": process.env,
+    },
+    build: {
+      outDir,
+      emptyOutDir: true,
+      lib: {
+        entry: "src/index.ts",
+        name: pluginEntryName,
+        formats: ["iife"],
+        fileName: () => "main.js",
+      },
+      rollupOptions: {
+        external: [
+          "vue",
+          "vue-router",
+          "@vueuse/core",
+          "@vueuse/components",
+          "@vueuse/router",
+          "@halo-dev/shared",
+          "@halo-dev/components",
+        ],
+        output: {
+          globals: {
+            vue: "Vue",
+            "vue-router": "VueRouter",
+            "@vueuse/core": "VueUse",
+            "@vueuse/components": "VueUse",
+            "@vueuse/router": "VueUse",
+            "@halo-dev/console-shared": "HaloConsoleShared",
+            "@halo-dev/components": "HaloComponents",
+          },
+          extend: true,
+        },
+      },
+    },
+  });
+};
