44a3d3ce70bb4e0cf76655bbac8ad94229785f4f
Ryan Wang <i@ryanc.cc>
2023-11-28 10:33:45 +0800

    chore: use @halo-dev/ui-plugin-bundler-kit to refactor vite config
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -4,58 +4,18 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import VueJsx from "@vitejs/plugin-vue-jsx";
 import Icons from "unplugin-icons/vite";
+import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
 
-const pluginEntryName = "PluginStarter";
-
-// https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const isProduction = mode === "production";
-  const outDir = isProduction
-    ? "../src/main/resources/console"
-    : "../build/resources/main/console";
-
-  return defineConfig({
-    plugins: [Vue(), VueJsx(), Icons({ compiler: "vue3" })],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [
+    Vue(),
+    VueJsx(),
+    Icons({ compiler: "vue3" }),
+    HaloUIPluginBundlerKit(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    define: {
-      "process.env": process.env,
-    },
-    build: {
-      outDir,
-      emptyOutDir: true,
-      lib: {
-        entry: "src/index.ts",
-        name: pluginEntryName,
-        formats: ["iife"],
-        fileName: () => "main.js",
-      },
-      rollupOptions: {
-        external: [
-          "vue",
-          "vue-router",
-          "@vueuse/core",
-          "@vueuse/components",
-          "@vueuse/router",
-          "@halo-dev/shared",
-          "@halo-dev/components",
-        ],
-        output: {
-          globals: {
-            vue: "Vue",
-            "vue-router": "VueRouter",
-            "@vueuse/core": "VueUse",
-            "@vueuse/components": "VueUse",
-            "@vueuse/router": "VueUse",
-            "@halo-dev/console-shared": "HaloConsoleShared",
-            "@halo-dev/components": "HaloComponents",
-          },
-          extend: true,
-        },
-      },
-    },
-  });
-};
+  },
+});
