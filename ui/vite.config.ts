d8ad0faad738e7c0236fca5fd96bcae3b06dfd7a
Ryan Wang <i@ryanc.cc>
2024-07-02 11:50:57 +0800

    chore: update infrastructure for UI
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,18 +1,12 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import VueJsx from "@vitejs/plugin-vue-jsx";
-import Icons from "unplugin-icons/vite";
 import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
+import Vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-    VueJsx(),
-    Icons({ compiler: "vue3" }),
-    HaloUIPluginBundlerKit(),
-  ],
+  plugins: [Vue(), Icons({ compiler: "vue3" }), HaloUIPluginBundlerKit()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
