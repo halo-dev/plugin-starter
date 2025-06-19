4172acfe747f03c5d0d47eea7ccb06afd7ed5e7a
Ryan Wang <i@ryanc.cc>
2025-06-19 17:02:41 +0800

    chore: update ui template
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import confetti from "canvas-confetti";
-import { onMounted } from "vue";
-import RiShareCircleLine from "~icons/ri/share-circle-line";
-import RiCodeBoxLine from "~icons/ri/code-box-line";
-import RiBookReadLine from "~icons/ri/book-read-line";
-import RiComputerLine from "~icons/ri/computer-line";
-import RiArrowRightSLine from "~icons/ri/arrow-right-s-line";
+import confetti from 'canvas-confetti'
+import { onMounted } from 'vue'
+import RiShareCircleLine from '~icons/ri/share-circle-line'
+import RiCodeBoxLine from '~icons/ri/code-box-line'
+import RiBookReadLine from '~icons/ri/book-read-line'
+import RiComputerLine from '~icons/ri/computer-line'
+import RiArrowRightSLine from '~icons/ri/arrow-right-s-line'
 
 onMounted(() => {
   confetti({
     particleCount: 100,
     spread: 70,
     origin: { y: 0.6, x: 0.58 },
-  });
-});
+  })
+})
 </script>
 
 <template>
@@ -28,9 +28,7 @@ onMounted(() => {
           target="_blank"
         >
           <h2 class="docs__box-title"><RiShareCircleLine />发布一个插件</h2>
-          <span class="docs__box-message">
-            了解如何与我们的社区分享您的扩展。
-          </span>
+          <span class="docs__box-message"> 了解如何与我们的社区分享您的扩展。 </span>
           <span class="docs__box-arrow">
             <RiArrowRightSLine />
           </span>
@@ -41,9 +39,7 @@ onMounted(() => {
           target="_blank"
         >
           <h2 class="docs__box-title"><RiComputerLine />基础概览</h2>
-          <span class="docs__box-message">
-            了解插件的项目结构、生命周期、资源配置等。
-          </span>
+          <span class="docs__box-message"> 了解插件的项目结构、生命周期、资源配置等。 </span>
           <span class="docs__box-arrow">
             <RiArrowRightSLine />
           </span>
@@ -115,8 +111,7 @@ onMounted(() => {
       transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
       transition-duration: 300ms;
       cursor: pointer;
-      filter: drop-shadow(0 1px 2px rgb(0 0 0 / 0.1))
-        drop-shadow(0 1px 1px rgb(0 0 0 / 0.06));
+      filter: drop-shadow(0 1px 2px rgb(0 0 0 / 0.1)) drop-shadow(0 1px 1px rgb(0 0 0 / 0.06));
 
       &:hover {
         box-shadow:
@@ -156,8 +151,7 @@ onMounted(() => {
       &:hover {
         .docs__box-arrow {
           color: #9ca3af;
-          transform: translate(00.375rem, 0) rotate(0) skewX(0) skewY(0)
-            scaleX(1) scaleY(1);
+          transform: translate(00.375rem, 0) rotate(0) skewX(0) skewY(0) scaleX(1) scaleY(1);
         }
       }
     }
