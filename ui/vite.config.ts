c81ec5a2ae968f2a6d5d8386739951ce8c67148a
Ryan Wang <i@ryanc.cc>
2025-06-20 17:00:00 +0800

    chore: update ui template (#53)
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,17 +1,25 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from 'url'
 
-import { viteConfig } from "@halo-dev/ui-plugin-bundler-kit";
-import Icons from "unplugin-icons/vite";
+import { viteConfig } from '@halo-dev/ui-plugin-bundler-kit'
+import Icons from 'unplugin-icons/vite'
+import { configDefaults } from 'vitest/config'
 
 // For more info,
 // please see https://github.com/halo-dev/halo/tree/main/ui/packages/ui-plugin-bundler-kit
 export default viteConfig({
   vite: {
-    plugins: [Icons({ compiler: "vue3" })],
+    plugins: [Icons({ compiler: 'vue3' })],
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
+
+    // If you don't use Vitest, you can remove the following configuration
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
   },
-});
+})
